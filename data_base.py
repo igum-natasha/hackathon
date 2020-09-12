6f0a627a7297b4f8b7d6717748ca05164f44d9d5
natasha <igum.natasha@gmail.com>
2020-09-12 15:04:52 +0300

    Add functions for work with db

--- data_base.py
+++ data_base.py
@@ -1,3 +1,30 @@
+from pymongo import MongoClient
+import pandas as pd
+
+
 class DataBase:
-    pass
+    def __init__(self):
+        self.client = MongoClient("mongodb://localhost:27017/")
+        self.file = '' #csv file name
+        self.data = []
+
+    def write_db(self, child: str):
+        db = self.client.test
+        if child not in db.list_collection_names():
+            data_child = db[child]
+            df = pd.read_csv(self.file)
+            records_ = df.to_dict(orient='records')
+            data_child.insert_many(records_)
+
+    def find_doc(self, child: str):
+        db = self.client.test
+        if child in db.list_collection_names():
+            results = 1
+        else:
+            results = -1
+        for x in db[child].find():
+            self.data.append(x)
+        if results != -1:
+            return self.data
+        return 0
 
